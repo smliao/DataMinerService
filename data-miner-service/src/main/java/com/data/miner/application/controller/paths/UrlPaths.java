56ec76ae44e9de80226ade98b9bc28c793a32974
St2s6b6 <steven_liao@gap.com>
2016-04-07 13:05:14 -0700

    Set up multi project

--- data-miner-service/src/main/java/com/data/miner/application/controller/paths/UrlPaths.java
+++ data-miner-service/src/main/java/com/data/miner/application/controller/paths/UrlPaths.java
@@ -3,7 +3,7 @@ package com.data.miner.application.controller.paths;
 
 public class UrlPaths {
 
-    public static final String ROOT_PATH = "/dataminer";
+    public static final String ROOT_PATH = "/data-miner-service";
     public static final String TEST = "/test";
     public static final String CREATE_PROFILE = "/user";
 }
