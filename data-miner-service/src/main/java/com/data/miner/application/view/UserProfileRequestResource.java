a3903d538018d1b65ac46d2265009b0d89907927
St2s6b6 <steven_liao@gap.com>
2016-06-22 20:57:02 -0700

    fix validation library

--- data-miner-service/src/main/java/com/data/miner/application/view/UserProfileRequestResource.java
+++ data-miner-service/src/main/java/com/data/miner/application/view/UserProfileRequestResource.java
@@ -1,12 +1,12 @@
 package com.data.miner.application.view;
 
 import com.data.miner.domain.UserProfile;
-import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @AllArgsConstructor
